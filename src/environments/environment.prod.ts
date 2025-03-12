b1068539ab777df84d174c83f963faf18a943c2a
Sundaram Rai <sundaramrai02@gmail.com>
2025-03-12 10:23:00 +0530

    fix production issue in environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: "https://juris-backend-eirw.onrender.com"
+  apiUrl: "https://juris-backend-eirw.onrender.com/api",
 };
