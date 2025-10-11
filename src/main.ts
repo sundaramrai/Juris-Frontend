04659e899d44940752443dad77b12a0462815bfe
Sundaram Rai <sundaramrai02@gmail.com>
2025-10-11 17:54:42 +0530

    fix: suppress console output in production environment

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,16 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app-routing.module';
 import { authInterceptor } from './app/interceptors/auth.interceptor';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  console.log = () => {};
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  console.warn = () => {};
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  console.error = () => {};
+}
 
 bootstrapApplication(AppComponent, {
   providers: [
