94414578750b9d2329e84061a7400ced6c3350e7
Sundaram Rai <sundaramrai02@gmail.com>
2025-10-11 17:31:18 +0530

    fix: include NgFor in HomeComponent imports for template rendering

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,12 +3,13 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { MatIconModule } from '@angular/material/icon';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  imports: [MatIconModule]
+  imports: [MatIconModule, NgFor]
 })
 export class HomeComponent {
   constructor(private router: Router, private authService: AuthService) { }
