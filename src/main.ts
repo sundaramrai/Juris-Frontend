e6c5425199706ee19150100624a192579ff93c25
Sundaram Rai <sundaramrai02@gmail.com>
2025-10-11 17:13:36 +0530

    refactor: migrate to bootstrapApplication for improved app initialization

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import 'zone.js';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app-routing.module';
+import { authInterceptor } from './app/interceptors/auth.interceptor';
 
-platformBrowserDynamic().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true,
-})
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideRouter(routes),
+    provideAnimations()
+  ]
+}).catch(err => console.error(err));
