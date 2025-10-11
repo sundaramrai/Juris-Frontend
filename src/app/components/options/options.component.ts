eafca4173df9a221ffc4463ad10fffc4d1525e99
Sundaram Rai <sundaramrai02@gmail.com>
2025-10-11 17:21:05 +0530

    fix: update OptionsComponent to utilize standalone imports for improved modularity

--- src/app/components/options/options.component.ts
+++ src/app/components/options/options.component.ts
@@ -1,10 +1,12 @@
 // src/app/components/options/options.component.ts
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-options',
-  standalone: false,
+  imports: [CommonModule, RouterModule],
   templateUrl: './options.component.html',
-  styleUrl: './options.component.css'
+  styleUrls: ['./options.component.css']
 })
 export class OptionsComponent { }
