636893fb639a2109cef8dedacdeb42d26ec16cac
Sundaram Rai <sundaramrai02@gmail.com>
2026-07-19 17:33:29 +0530

    fix: enhance AuthInterceptor to handle HTTP errors and unauthorized responses

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,5 +1,6 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
@@ -12,5 +13,12 @@ export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
       }
     });
   }
-  return next(req);
+  return next(req).pipe(
+    catchError((error: unknown) => {
+      if (error instanceof HttpErrorResponse && [401, 403].includes(error.status)) {
+        authService.handleUnauthorized();
+      }
+      return throwError(() => error);
+    })
+  );
 };
